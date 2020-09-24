1359f39cc1b2163075ad7b896cb71373896b2854
jhw2167 <jhw2167@columbia.edu>
2020-09-23 23:33:23 -0700

    finished gcd.c, gcd.h and incorporated into main

--- part1/gcd.c
+++ part1/gcd.c
@@ -3,5 +3,34 @@
 
 int findGCD(int x, int y)
 {
-	
+	/*
+		Function will make use of a while loop to determine the GCD
+		and determine coprime relation and return that value
+	*/
+
+	if ((x == 0) || (y == 0))
+		return 0;
+
+	//assign gcd to minimum of x & y
+	int gcd = 1;
+	int larger = 1;
+
+	if (x > y) {
+		larger = x;
+		gcd = y;
+	}
+	else {
+		larger = y;
+		gcd = x;
+	}
+
+	int remainder = (larger % gcd);
+
+	while (remainder != 0) {
+		larger = gcd;
+		gcd = remainder;
+		remainder = (larger % gcd);
+	}
+
+	return gcd;
 }
