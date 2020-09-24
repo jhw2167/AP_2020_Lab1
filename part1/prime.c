94ad43d135110bccb46497dea685a67f5809cf57
jhw2167 <jhw2167@columbia.edu>
2020-09-23 23:32:06 -0700

    finished prime.c, prime.h

--- part1/prime.c
+++ part1/prime.c
@@ -3,5 +3,32 @@
 
 _Bool isPrime(int test)
 {
-	
+	/*
+		Function will test if a passed int "test"
+		is prime using the modulo operator
+		we return a bool
+	*/
+
+	//0 and 1 are not prime numbers so lets
+	//test for them
+	if ((test == 0) || (test == 1))
+		return false;
+
+	//half test is the greatest number that
+	//test could be divisible by
+	int half_test = test / 2;
+	int divisor = 2;
+
+	while (divisor <= half_test)
+	{
+		//if the modulo is 0, we dont have a prime
+		if ((test % divisor) == 0) {
+			return false;
+		}
+		else
+			divisor++;
+	}
+
+	//if we didnt find a divisor, must be prime
+	return true;
 }
